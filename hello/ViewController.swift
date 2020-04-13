3fe75b41f74bdce625927f9c4255dc42817cfb56
Shelly <shelly980102@gmail.com>
2020-04-13 15:10:48 +0800

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by 蕭聿莘 on 2020/4/13.
+//  Copyright © 2020 Kazuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
