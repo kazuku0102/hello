87c6c7a830b761a8ca93169e2845acb585fddfed
Shelly <shelly980102@gmail.com>
2020-04-16 15:45:20 +0800

    excercise

--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        let textImage = UIImage(named: "sunglasses")//讀取的圖片
+        let textImageView = UIImageView(image: textImage)//將圖片加入ImageView
+        let url = URL(string: "https://bit.ly/2yepDbb")
+        let player = AVPlayer(url: url!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(playerLayer)
+        playerLayer.frame = CGRect(origin: CGPoint(x: 2, y: 300), size: textImageView.frame.size)
+        playerLayer.mask = textImageView.layer
+        player.play()
+        
+        
+//        let hatImageView = UIImageView(image: UIImage(named: "hat"))
+        let hatImage = UIImage(named: "hat")
+        let hatImageView = UIImageView(image: hatImage)
+        hatImageView.frame = CGRect(origin: CGPoint(x: 55, y: 50), size: hatImageView.frame.size)
 
+        let starImage = UIImage(named: "star")
+        let starImageView = UIImageView(image: starImage)
+        starImageView.mask = hatImageView
+        self.view.addSubview(starImageView)
+        
 }
-
+    
+}
